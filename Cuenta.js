ac84667c3178f9f573cfc8e6f4dcc029ef1cbd7b
Cyberkrane <cyberkrane@gmail.com>
2021-09-13 12:33:23 -0300

    carga inicial de archivos
    carga inicial de archivos

---
+++ Cuenta.js
@@ -0,0 +1,30 @@
+ var end = new Date('10/15/2021 15:00');
+
+ var _second = 1000;
+ var _minute = _second * 60;
+ var _hour = _minute * 60;
+ var _day = _hour * 24;
+ var timer;
+
+ function showRemaining() {
+     var now = new Date();
+     var distance = end - now;
+     if (distance < 0) {
+
+         clearInterval(timer);
+         document.getElementById('countdown').innerHTML = 'EL MOMENTO HA LLEGADO!!!';
+
+         return;
+     }
+     var days = Math.floor(distance / _day);
+     var hours = Math.floor((distance % _day) / _hour);
+     var minutes = Math.floor((distance % _hour) / _minute);
+     var seconds = Math.floor((distance % _minute) / _second);
+
+     document.getElementById('countdown').innerHTML = days + ' dias, ';
+     document.getElementById('countdown').innerHTML += hours + ' horas, ';
+     document.getElementById('countdown').innerHTML += minutes + ' minutos y ';
+     document.getElementById('countdown').innerHTML += seconds + ' segundos';
+ }
+
+ timer = setInterval(showRemaining, 1000);
